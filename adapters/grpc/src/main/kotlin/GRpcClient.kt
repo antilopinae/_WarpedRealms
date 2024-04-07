a7fa36f1876756e31ccfd38ec71e49f69beab6ad
antilopa <mikbas31@gmail.com>
2024-04-07 19:22:20 +0300

    Initial commit

--- adapters/grpc/src/main/kotlin/GRpcClient.kt
+++ adapters/grpc/src/main/kotlin/GRpcClient.kt
@@ -1,7 +1,28 @@
 package adapters.grpc.client
 
+import com.grpc.GreetingServiceGrpc
+import com.grpc.GreetingServiceOuterClass
+import io.grpc.ManagedChannel
+import io.grpc.ManagedChannelBuilder
+
 class GRpcClient {
     init{
         println("GRpc client started")
+
+    }
+    val channel: ManagedChannel =
+        ManagedChannelBuilder.forTarget("localhost:8080")
+        .usePlaintext()
+        .build()
+    val stub: GreetingServiceGrpc.GreetingServiceBlockingStub =
+        GreetingServiceGrpc.newBlockingStub(channel)
+    fun sendMessage(){
+        val request = GreetingServiceOuterClass.HelloRequest
+            .newBuilder()
+            .setName("Client1")
+            .build()
+        val response: GreetingServiceOuterClass.HelloResponse = stub.greeting(request)
+        println("$response")
+        channel.shutdownNow()
     }
 }
--- adapters/src/main/kotlin/ServerConnector.kt
+++ adapters/src/main/kotlin/ServerConnector.kt
@@ -2,18 +2,21 @@ package adapters
 
 import adapters.grpc.client.GRpcClient
 import adapters.request.ServerRequest
+import io.ktor.client.plugins.*
 
 //Подключение к серверу, передача событий.
 class ServerConnector() {
     private val serverRequest = ServerRequest()
     //private val server_builder = ServerBuilder().also { serverRequest.serverBuilder = it }
-
+    val client = GRpcClient()
     init {
         println("==========Server To Connect==========")
         //serverRequest.startConnection()
-        val server = GRpcClient()
+        send()
     }
-
+    fun send(){
+        client.sendMessage()
+    }
     fun push(p: ByteArray): Boolean {
         return true
     }
