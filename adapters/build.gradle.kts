be25c5c1700a4ecdb3984b6ac9abb448f9b424a2
antilopa <mikbas31@gmail.com>
2024-04-07 17:36:42 +0300

    Initial commit

--- adapters/build.gradle.kts
+++ adapters/build.gradle.kts
@@ -1,19 +1,22 @@
-val ktor_version: String by project
+val ktorVersion: String by project
 val ktxVersion: String by project
 val kotlinxCoroutinesVersion: String by project
 val gdxVersion: String by project
 val kotlinVersion: String by project
 
+group = "adapters"
+version = "unspecified"
+
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     api("io.github.libktx:ktx-log:$ktxVersion")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:${kotlinxCoroutinesVersion}")
-    implementation("io.ktor:ktor-client-core:$ktor_version")
-    implementation("io.ktor:ktor-client-json:$ktor_version")
-    implementation("io.ktor:ktor-client-serialization:$ktor_version")
-    implementation("io.ktor:ktor-client-logging:$ktor_version")
-    implementation("io.ktor:ktor-client-cio:$ktor_version")
-    implementation("io.ktor:ktor-client-websockets:$ktor_version")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-json:$ktorVersion")
+    implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+    implementation("io.ktor:ktor-client-logging:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-websockets:$ktorVersion")
     implementation(project(":adapters:grpc"))
 }
 
--- adapters/grpc/build.gradle.kts
+++ adapters/grpc/build.gradle.kts
@@ -1,3 +1,11 @@
+import com.google.protobuf.gradle.id
+
+val ktorVersion: String by project
+val grpcKotlinVersion: String by project
+val grpcVersion: String by project
+val protobufVersion: String by project
+val protobufPluginVersion: String by project
+
 group = "adapters:grpc"
 version = "unspecified"
 
@@ -7,6 +15,48 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    //serialization
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-jackson-jvm:$ktorVersion")
+
+    //grpc
+    implementation("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
+
+    implementation("io.grpc:grpc-stub:$grpcVersion")
+    implementation("io.grpc:grpc-protobuf:$grpcVersion")
+    implementation("com.google.protobuf:protobuf-kotlin:$protobufVersion")
+
+    implementation("io.grpc:grpc-netty:$grpcVersion")
+    implementation("io.grpc:grpc-netty-shaded:$grpcVersion")
+
+//    implementation("io.grpc:protoc-gen-grpc-java:$grpcVersion")
+//    implementation("io.grpc:protoc-gen-grpc-kotlin:$grpcKotlinVersion:jdk8@jar")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:$protobufVersion"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:$grpcKotlinVersion:jdk8@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            if (it.name.startsWith("generateTestProto")) {
+                it.dependsOn("jar")
+            }
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+        }
+    }
 }
 
 tasks.test {
